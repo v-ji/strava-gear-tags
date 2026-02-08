b157d33680bad41bb03945db968af0c8b9f8ef70
Viktor Illmer <1476338+v-ji@users.noreply.github.com>
2026-02-08 19:02:24 +0100

    Disable access logs

--- src/main.py
+++ src/main.py
@@ -15,9 +15,11 @@ app.include_router(health.router)
 host = os.getenv("HOST", "127.0.0.1")
 port = int(os.getenv("PORT", 8000))
 
+
 def main():
     """Entry point for running the FastAPI application with uvicorn."""
-    uvicorn.run(app, host=host, port=port, log_level="info")
+    uvicorn.run(app, host=host, port=port, log_level="info", access_log=False)
+
 
 if __name__ == "__main__":
     main()
